0d0d214de9572c9e151d50efd34cffcde21df0e4
Lucas Gomes <lucasggomees@gmail.com>
2023-10-29 00:22:34 +0100

    Add default state for the board

---
+++ src/Words.js
@@ -0,0 +1,14 @@
+/* 
+Initial board state. It's an array of arrays - matrix.
+Each array has five empty string as starting value.
+Each string will correspond to a letter
+Each array will correspond to an attempt
+*/
+export const boardDefault = [
+  new Array(5).fill(""),
+  new Array(5).fill(""),
+  new Array(5).fill(""),
+  new Array(5).fill(""),
+  new Array(5).fill(""),
+  new Array(5).fill(""),
+]
