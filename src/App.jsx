ea3c16017c314cacb15114cce2bcd004883cf6f0
Lucas Gomes <lucasggomees@gmail.com>
2023-10-29 00:52:50 +0100

    Fix nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <div className='App'>
-      <nav>WordHunt</nav>
+      <nav>
+        <h1>WordHunt</h1>
+      </nav>
       <Board />
       <Keyboard />
     </div>
