594322b8cddf98ead122a7e7b32f3573ba856a4e
Lucas Gomes <lucasggomees@gmail.com>
2023-10-29 17:22:26 +0000

    Add a state to manage current guesses

--- src/contexts/BoardStateProvider.jsx
+++ src/contexts/BoardStateProvider.jsx
@@ -1,5 +1,3 @@
-import { createContext, useState } from 'react'
-
 /* 
 Initial board state. It's an array of arrays - matrix.
 Each array has five empty string as starting value - a word has five letters.
@@ -7,17 +5,24 @@ Each string will correspond to a letter
 Each array will correspond to an attempt
 */
 
-// This creates an array with 6 elements, and for each element, it creates a new array with 5 empty strings
-const boardDefault = Array.from({ length: 6 }, () => Array(5).fill(''))
+import { createContext, useState } from 'react'
 
 // Create a react context for board
 export const BoardContext = createContext()
 
 export function BoardStateProvider({ children }) {
+  // This creates an array with 6 elements, and for each element, it creates a new array with 5 empty strings
+  const boardDefault = Array.from({ length: 6 }, () => Array(5).fill(''))
+  // Initial guess object
+  const intialGuess = { attempt: 0, letterPos: 0 }
+
   // State to manage board value. It will be passed down to children components
   const [board, setBoard] = useState(boardDefault)
+  // State to manage current guess
+  const [guess, setGuess] = useState(intialGuess)
+
   return (
-    <BoardContext.Provider value={{ board, setBoard }}>
+    <BoardContext.Provider value={{ board, setBoard, guess, setGuess }}>
       {children}
     </BoardContext.Provider>
   )
