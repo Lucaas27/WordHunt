8557e0aee626ba7e0d03ca4e36cbc1a398f52df5
Lucas Gomes <lucasggomees@gmail.com>
2023-10-29 08:34:04 +0000

    Code refactoring

--- src/Words.js
+++ src/Words.js
@@ -1,14 +1,9 @@
 /* 
 Initial board state. It's an array of arrays - matrix.
-Each array has five empty string as starting value.
+Each array has five empty string as starting value - a word has five letters.
 Each string will correspond to a letter
 Each array will correspond to an attempt
 */
-export const boardDefault = [
-  new Array(5).fill(""),
-  new Array(5).fill(""),
-  new Array(5).fill(""),
-  new Array(5).fill(""),
-  new Array(5).fill(""),
-  new Array(5).fill(""),
-]
+
+// This creates an array with 6 elements, and for each element, it creates a new array with 5 empty strings
+export const boardDefault = Array.from({ length: 6 }, () => new Array(5).fill(""));
