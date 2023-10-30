92d9d693a0ce1b340cf14b7f60f6a341d9d2e69a
Lucas Gomes <lucasggomees@gmail.com>
2023-10-30 21:29:34 +0000

    Add feature to deal with key presses in the keyboard
    UseEffect is used to set up a keyboard event listener when the component mounts and uses handleKeyDown as the dependency. It is a function that responds to keyboard events

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useCallback, useContext } from 'react'
 import Key from './Key'
+import { BoardContext } from '../contexts/BoardStateProvider'
 
 function Keyboard() {
   // Create three arrays for each row of the keyboard
@@ -6,8 +8,42 @@ function Keyboard() {
   const keysSecondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
   const keysThirdRow = ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DELETE']
 
+  const { board, onDelete, onEnter, onSelectLetter } = useContext(BoardContext)
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === 'Enter') {
+        onEnter()
+      } else if (event.key === 'Backspace' || event.key === 'Delete') {
+        onDelete()
+      } else {
+        // Create an array containing all keys, excluding 'ENTER' and 'DELETE'
+        const keys = [
+          ...keysFirstRow,
+          ...keysSecondRow,
+          ...keysThirdRow.filter((key) => key !== 'ENTER' && key !== 'DELETE'),
+        ]
+
+        /* 
+      Check if array includes the letter being pressed.
+      If it does, call onSelectLetter with the value being keyed.
+      */
+        keys.includes(event.key.toUpperCase()) &&
+          onSelectLetter(event.key.toUpperCase())
+      }
+    },
+    [board]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
   return (
-    <div className='keyboard'>
+    <div className='keyboard' onKeyDown={handleKeyDown}>
       <div className='first-row'>
         {keysFirstRow.map((key, index) => (
           <Key keyVal={key} key={index} />
