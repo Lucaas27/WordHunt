dc6029603ebb5e8d66a4937c6dd2083d9ff81a06
Lucas Gomes <lucasggomees@gmail.com>
2023-11-06 22:40:02 +0000

    Code refactoring

--- server/controllers/randomWords.js
+++ server/controllers/randomWords.js
@@ -1,10 +1,19 @@
 
 export const getRandomWords = async (req, res) => {
   try {
+    // Call external API
     const response = await fetch(process.env.WORDS_API_URL)
     const data = await response.json()
-    return res.json(data)
+
+    // If response = 200. return the data as it is
+    if (response.status === 200) {
+      // Send the data to the client
+      return res.json(data);
+    } else {
+      return res.status(response.status).json({ error: 'External API error' });
+    }
   } catch (error) {
-    return console.error(error)
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
   }
 }
